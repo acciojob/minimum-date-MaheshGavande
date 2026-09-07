9b1a6ad137e2d4deac7ad78de8b3d6cca9ccfd98
MaheshGavande <gawandemahesh420@gmail.com>
2026-09-07 14:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function minDate(dates) {
   //write you code here
+	let yearMin=3000;
+	for (let date of dates) {
+		 yearMin = Math.min(yearMin , date.slice(0,4));
+	}
+	let newArr =[];
+	for (let date of dates) {
+		if (yearMin == date.slice(0,4)) {
+			newArr.push(date);
+		}
+	}
+	let monthMin =3000;
+	for (let arr of newArr) {
+		monthMin = Math.min(monthMin, arr.slice(5,7));
+	}
+	let newArr2 = [];
+	for (let arr of newArr) {
+		if (monthMin == arr.slice(5,7)) {
+			newArr2.push(arr);
+		}
+	}
+
+	let dateMin =3000;
+	for (let arr of newArr2) {
+		dateMin = Math.min(dateMin, arr.slice(7,9));
+	}
+	
+	for (let arr of newArr2) {
+		if(dateMin == arr.slice(7,9)) {
+			return arr;
+		} 
+	}
+	return -1;
+	
 }
 
 // Do not change the code
@@ -35,6 +68,7 @@ var dates = [
   "2023/03/28",
   "2023/03/29",
   "2023/03/30",
-];
+];  
+
 
 alert(minDate(dates));
